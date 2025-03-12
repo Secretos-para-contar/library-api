7d4cf8b4dfc50915a3943911dd86e1d515cbb4d8
d4lion <d4lion.dev@gmail.com>
2025-03-12 11:08:21 -0500

    🚧 Required(Refactor): LibrosController and Program.cs for future dependency injection setup

--- SecretosParaContar.API/Controllers/LibrosController.cs
+++ SecretosParaContar.API/Controllers/LibrosController.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
-using SecretosParaContar.Business.interfaces;
-using SecretosParaContar.Business.Service;
-using SecretosParaContar.Data.Models;
-using SecretosParaContar.Data;
+// using SecretosParaContar.Business.interfaces;
+// using SecretosParaContar.Business.Service;
+// using SecretosParaContar.Data.Models;
+// using SecretosParaContar.Data;
 
 namespace SecretosParaContar.API.Controller
+
+// TODO: Despues de hacer la inyección de dependencias y patron singleton descomentar las siguientes lineas
+// Y generar la inyección de dependencias en el constructor
+
 
 {
     [Route("Api/[controller]")]
@@ -13,40 +17,42 @@ namespace SecretosParaContar.API.Controller
     public class LibrosController : ControllerBase
 
     {
-        private readonly ILibroService _libroService;
+        // private readonly ILibroService _libroService;
 
-        public LibrosController(ILibroService libroService)
-        {
-            _libroService = libroService;
-        }
+        // public LibrosController(ILibroService libroService)
+        // {
+        //     _libroService = libroService;
+        // }
+
         [HttpGet]
-        [Route("GetAllLibros")]
+        [Route("GetLibros")]
         
         public async Task<IActionResult> GetLibros()
         {
-            var libros = await _libroService.GetList();
-            return Ok(libros);
+            // var libros = await _libroService.GetList();
+            // return Ok(libros);
+            return Ok("Hola");
         }
         
-        [HttpPost]
-        [Route("AddLibros")]
+        // [HttpPost]
+        // [Route("AddLibros")]
         
-        public async Task<IActionResult> AddLibros(Libros libro)
-        {
-            var result = await _libroService.AddLibro(libro);
-            return Ok(result);
-        }
+        // public async Task<IActionResult> AddLibros(Libros libro)
+        // {
+        //     var result = await _libroService.AddLibro(libro);
+        //     return Ok(result);
+        // }
 
-        [HttpGet("GetLibrosName")]
+        // [HttpGet("GetLibrosName")]
         
-        public async Task<IActionResult> GetLibrosName(string name)
-        {
-            var result = await _libroService.FindByName(name);
+        // public async Task<IActionResult> GetLibrosName(string name)
+        // {
+        //     var result = await _libroService.FindByName(name);
 
-            return result.TotalElements > 0 ?
-            Ok(result) :
-            NotFound(result);
-        }
+        //     return result.TotalElements > 0 ?
+        //     Ok(result) :
+        //     NotFound(result);
+        // }
 
     }
 }
