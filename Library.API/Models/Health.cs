46a80cdcf4c22773c6e729e690a12f12467fa26c
d4lion <d4lion.dev@gmail.com>
2025-03-21 00:38:40 -0500

    ✨ Add HealthCheck API with HealthResponse model and Health data model

---
+++ Library.API/Models/Health.cs
@@ -0,0 +1,17 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace Library.API.Models;
+
+[Table("health")]
+public class Health : BaseModel
+{
+    [PrimaryKey("id", false)]
+    public long Id { get; set; }
+    
+    [Column("author")]
+    public string Author { get; set; } = string.Empty;
+    
+    [Column("create_at")]
+    public DateTime CreateAt { get; set; }
+}
